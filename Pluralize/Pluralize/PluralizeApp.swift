2a984d8926a0af78ca7fe2173c68b7eaff31ba5c
Adolpho Piazza <adolphopiazza@gmail.com>
2023-09-23 23:42:21 -0300

    Initial commit

---
+++ Pluralize/Pluralize/PluralizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PluralizeApp.swift
+//  Pluralize
+//
+//  Created by Adolpho Francisco Zimmermann Piazza on 23/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PluralizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
